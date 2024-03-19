4ef50c8d03232116b8a9d3924e89efb0c6723163
Michal <hardcorpl5@gmail.com>
2024-03-19 22:38:20 +0100

    finish hamburger menu

--- assets/javascript/hamburger.js
+++ assets/javascript/hamburger.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav');
+
+    menu_btn.addEventListener('click', () => {
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    });
 });
