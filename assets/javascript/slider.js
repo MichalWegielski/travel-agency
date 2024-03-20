25be4460c6ce313778dfe91f6e15338e57462fb2
Michal <hardcorpl5@gmail.com>
2024-03-20 22:42:18 +0100

    Finish modal

--- assets/javascript/slider.js
+++ assets/javascript/slider.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const heading = document.querySelector('.caption h1');
     const description = document.querySelector('.caption p');
     const tooltip = document.querySelector('.helper');
+    const header = document.querySelector('.tour-header');
+    const descriptionTour = document.querySelector('.tour-description');
+    const img = document.querySelector('.tour-img');
 
     const images = ["zdjecie1.jpg", "zdjecie2.jpg", "zdjecie3.jpg", "zdjecie4.jpg", "zdjecie5.jpg"];
 
@@ -34,6 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         slide(id);
+        changeData(id);
     });
 
     arrRight.addEventListener('click', () => {
@@ -44,6 +48,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         slide(id);
+        changeData(id);
     });
 
     slider.addEventListener('mouseenter', () => {
@@ -53,4 +58,34 @@ document.addEventListener('DOMContentLoaded', () => {
     slider.addEventListener('mouseleave', () => {
         tooltip.classList.remove('tooltip');
     });
+
+    function changeData(id) {
+        if(id === 0) {
+            header.innerHTML = "Turcja";
+            descriptionTour.innerHTML = "Ciepłe, piaszczyste wybrzeża i niesamowite krajobrazy";
+            img.src ='../assets/images/zdjecie1.jpg';
+        } else if (id === 1) {
+            header.innerHTML = "Chorwacja";
+            descriptionTour.innerHTML = "Urocze i tętniące życiem stare promenady";
+            img.src ='../assets/images/zdjecie2.jpg';
+        } else if (id === 2) {
+            header.innerHTML = "Grecja";
+            descriptionTour.innerHTML = "Spokój i cisza w śródziemnomorskim klimacie";
+            img.src ='../assets/images/zdjecie3.jpg';
+        } else if (id === 3) {
+            header.innerHTML = "Malediwy";
+            descriptionTour.innerHTML = "Odrobina raju na ziemi, u nas dla każdego";
+            img.src ='../assets/images/zdjecie4.jpg';
+        } else if (id === 4) {
+            header.innerHTML = "Seszele";
+            descriptionTour.innerHTML = "Krajobrazy nie z tego świata oraz dzikie i niezbadane zakątki";
+            img.src ='../assets/images/zdjecie5.jpg';
+        }
+    }
+
+    if(id === 0) {
+        header.innerHTML = "Turcja";
+        descriptionTour.innerHTML = "Ciepłe, piaszczyste wybrzeża i niesamowite krajobrazy";
+        img.src ='../assets/images/zdjecie1.jpg';
+    }
 });
